3499b23cad8097c65b7bda9eeb6733cdcaabc60e
ncclapyohands <nathancook648@gmail.com>
2023-02-03 23:10:19 -0700

    initial push

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongodb = require('./db');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+  .use('/', routes);
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
